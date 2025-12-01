1fc1cbcd2c1ba186c2ebe8d464a4435efc9a8e25
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2025-12-01 17:43:08 +0530

    Add Login services

---
+++ BSEB_CoreAPI/Services/ILoginService.cs
@@ -0,0 +1,9 @@
+﻿using BSEB_CoreAPI.Model;
+
+namespace BSEB_CoreAPI.Services
+{
+    public interface ILoginService
+    {
+        Task<LoginResult> LoginUserAsync(string username, string password);
+    }
+}
