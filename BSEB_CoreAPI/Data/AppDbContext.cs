99ad603a493a2ca6f6461dcd397f80bfa3b04de5
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2025-12-01 17:43:54 +0530

    add ds for login api

---
+++ BSEB_CoreAPI/Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using BSEB_CoreAPI.Model;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace BSEB_CoreAPI.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<CollegeUser> CollegeUsers { get; set; }
+        public DbSet<LoginUserInfo> LoginUserInfo { get; set; }
+        public DbSet<CollegeMst> CollegeMst { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           
+
+            // If "Users" is a DTO or view
+            modelBuilder.Entity<CollegeMst>().HasNoKey();
+            modelBuilder.Entity<LoginUserInfo>().HasNoKey();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
