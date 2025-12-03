2003f8921a167f0a4ee8fde553d0f3682aba293f
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2025-12-03 17:22:38 +0530

    Create CollegeMst.cs
    Introduced the CollegeMst class in the Model namespace to represent college details, including properties for college ID, code, name, principal information, location, contact, update date, and active status.

---
+++ BSEB_CoreAPI/Model/CollegeMst.cs
@@ -0,0 +1,16 @@
+﻿namespace BSEB_CoreAPI.Model
+{
+    public class CollegeMst
+    {
+        public int Pk_CollegeId { get; set; }
+        public string CollegeCode { get; set; }
+        public string CollegeName { get; set; }
+        public string PrincipalName { get; set; }
+        public string PrincipalMobileNo { get; set; }
+        public int Fk_BlockId { get; set; }
+        public int Fk_DistrictId { get; set; }
+        public string EmailId { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
