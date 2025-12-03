fe410950d563aad2ff3c9c43e6ba91676bdfa6b6
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2025-12-03 17:23:19 +0530

    Add LoginUserInfo model class
    Introduced the LoginUserInfo class to represent user login details, including college and district information, principal's mobile number, and email ID.

---
+++ BSEB_CoreAPI/Model/LoginUserInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace BSEB_CoreAPI.Model
+{
+    public class LoginUserInfo
+    {
+        public int CollegeId { get; set; }
+        public string UserName { get; set; }
+        public string CollegeName { get; set; }
+        public string CollegeCode { get; set; }
+        public string DistrictName { get; set; }
+        public string DistrictCode { get; set; }
+        public string PrincipalMobileNo { get; set; }
+        public string EmailId { get; set; }
+    }
+}
