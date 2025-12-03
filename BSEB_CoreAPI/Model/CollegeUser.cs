2e96103bd2df817bb2bddb06f714f36a1ed91ff7
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2025-12-03 17:22:54 +0530

    Add CollegeUser model class
    Introduces the CollegeUser class to represent college user data, including properties for identification, authentication, and metadata. Note: Plain text password storage is included but not recommended for production.

---
+++ BSEB_CoreAPI/Model/CollegeUser.cs
@@ -0,0 +1,19 @@
+﻿namespace BSEB_CoreAPI.Model
+{
+    public class CollegeUser
+    {
+        public int Id { get; set; }
+        public string DistrictCode { get; set; }
+        public string DistrictName { get; set; }
+        public string CollegeCode { get; set; }
+        public string CollegeName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public int Fk_CollegeId { get; set; }
+        public string PlainText_Password { get; set; } // Assuming you store the plain text password temporarily (not recommended for production)
+    }
+
+}
