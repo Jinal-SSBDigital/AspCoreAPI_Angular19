efe91b18ca1c4c4a80cb0137a6c1a22cce09a5b8
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2025-12-06 17:02:09 +0530

    Add IdwnldRegFormService interface
    Introduces the IdwnldRegFormService interface with a method to retrieve student details based on college and student information.

---
+++ BSEB_CoreAPI/Services/IdwnldRegFormService.cs
@@ -0,0 +1,9 @@
+﻿using BSEB_CoreAPI.Model;
+
+namespace BSEB_CoreAPI.Services
+{
+    public interface IdwnldRegFormService
+    {
+        Task <List<Student_Mst>> GetStudentDetails(string Collegeid,String CollegeCode,string StudentName,string FacultyId);
+    }
+}
