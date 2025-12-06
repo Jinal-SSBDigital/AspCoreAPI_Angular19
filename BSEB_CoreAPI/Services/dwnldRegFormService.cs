bc168aebbc65a29aaa06c853c063eb549be4d500
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2025-12-06 17:01:59 +0530

    Add dwnldRegFormService for student details retrieval
    Introduces dwnldRegFormService implementing IdwnldRegFormService, providing a method to fetch student details from the database using stored procedure 'sp_GetStudentDetails' with various parameters.

---
+++ BSEB_CoreAPI/Services/dwnldRegFormService.cs
@@ -0,0 +1,43 @@
+﻿using BSEB_CoreAPI.Data;
+using BSEB_CoreAPI.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Data.SqlClient;
+
+
+namespace BSEB_CoreAPI.Services
+{
+    public class dwnldRegFormService : IdwnldRegFormService
+    {
+        private readonly IConfiguration _configuration; 
+        private readonly AppDbContext _context;
+
+        public dwnldRegFormService(IConfiguration configuration, AppDbContext context)
+        {
+            _configuration = configuration;
+            _context = context;
+        }
+
+        public async Task<List<Student_Mst>> GetStudentDetails(string Collegeid, string CollegeCode, string StudentName, string FacultyId)
+        {
+            //throw new NotImplementedException();
+            try
+            {
+                var CollegeIdPr=new SqlParameter("@CollegeId", Collegeid);
+                var CollegCodePr=new SqlParameter("@CollegeCode", (object?)CollegeCode ?? DBNull.Value);
+                var StudentNamePr= new SqlParameter("@StudentName", (object?)StudentName ?? DBNull.Value);
+                var FacultyIdPr = new SqlParameter("@FacultyId", FacultyId);
+                var SubCategory = new SqlParameter("@SubCategory", DBNull.Value);
+
+                //var result = _context.StudentDetails.FromSqlRaw("EXEC sp_GetStudentDetails @CollegeId, @CollegeCode, @StudentName, @FacultyId,@SubCategory", CollegeIdPr, CollegCodePr, StudentNamePr, FacultyIdPr, SubCategory).AsEnumerable().FirstOrDefault();
+                var result = await _context.StudentDetails.FromSqlRaw("EXEC sp_GetStudentDetails @CollegeId, @CollegeCode, @StudentName, @FacultyId,@SubCategory", CollegeIdPr, CollegCodePr, StudentNamePr, FacultyIdPr, SubCategory).ToListAsync();
+              
+                return result;
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+    }
+}
