74ed9f1f7278a31206c61d4a2a8048fdb6739b4e
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2025-12-06 17:01:37 +0530

    Add dwnldRegFormController for student registration form
    Introduces dwnldRegFormController with a POST endpoint to download student registration form details. The controller uses IdwnldRegFormService to fetch student data based on provided request parameters.

---
+++ BSEB_CoreAPI/Controllers/dwnldRegFormController.cs
@@ -0,0 +1,36 @@
+﻿using BSEB_CoreAPI.Model;
+using BSEB_CoreAPI.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BSEB_CoreAPI.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class dwnldRegFormController : ControllerBase
+    {
+        private readonly IdwnldRegFormService _dwnldRegFormService;
+        public dwnldRegFormController(IdwnldRegFormService dwnldRegFormService)
+        {
+            _dwnldRegFormService = dwnldRegFormService;
+        }
+        [AllowAnonymous]
+        [HttpPost("dwnldRegForm")]
+        public async Task<IActionResult> dwnldRegForm([FromBody]StudentRequest Stu)
+        {
+            try
+            {
+                var result = await _dwnldRegFormService.GetStudentDetails(Stu.CollegeId.ToString(),Stu.CollegeCode,Stu.StudentName,Stu.FacultyId.ToString());
+                return Ok(new { success = true, data = result });
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { success = false, message = "Internal server error" });
+            }
+          
+        }
+    }
+}
