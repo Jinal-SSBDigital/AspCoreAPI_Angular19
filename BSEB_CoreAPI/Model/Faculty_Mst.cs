af0d4d6670c13583fd2335f7a12f76f57685d675
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2025-12-10 17:49:13 +0530

    Add Faculty_Mst model class
    Introduces the Faculty_Mst class with properties for faculty ID, name, and active status. This model will be used to represent faculty data in the application.

---
+++ BSEB_CoreAPI/Model/Faculty_Mst.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BSEB_CoreAPI.Model
+{
+    public class Faculty_Mst
+    {
+        //[Key]
+        public int Pk_FacultyId { get; set; }
+
+        public string? FacultyName { get; set; }
+
+        public bool? IsActive { get; set; }
+    }
+}
