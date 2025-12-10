24d5613d1079ac20efda1cbf29afb7e798aca83d
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2025-12-10 17:49:21 +0530

    Add Student_Mst model class
    Introduces the Student_Mst class to represent student data, including properties for student and parent names, date of birth, faculty, college, and form download status. This model will be used for managing student information in the application.

---
+++ BSEB_CoreAPI/Model/Student_Mst.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BSEB_CoreAPI.Model
+{
+    public class Student_Mst
+    {
+        [Key]
+        public int StudentID { get; set; }
+        public string? StudentName { get; set; }
+        public string? FatherName { get; set; }
+        public string? MotherName { get; set; }
+        public DateTime? DOB { get; set; }
+        //public string CategoryName { get; set; }
+        public string? Faculty { get; set; }
+        public int? FacultyId { get; set; }
+        public int? CollegeId { get; set; }
+        public string? College { get; set; }
+        //public string OfssReferenceNo { get; set; }
+        public bool? FormDownloaded { get; set; }
+
+    }
+}
