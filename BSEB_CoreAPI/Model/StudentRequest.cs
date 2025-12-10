6aca701704d96b67eed45f641436b6ee2d790250
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2025-12-10 17:49:33 +0530

    Add StudentRequest model class
    Introduces the StudentRequest class with properties for CollegeId, CollegeCode, StudentName, and faculty. This model will be used to handle student-related requests in the API.

---
+++ BSEB_CoreAPI/Model/StudentRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.Drawing.Imaging;
+
+namespace BSEB_CoreAPI.Model
+{
+    public class StudentRequest
+    {
+        public string? CollegeId { get; set; }
+
+        public string? CollegeCode { get; set; }
+
+        public string? StudentName { get; set; }
+        public int faculty { get; set; }
+       
+    }
+}
