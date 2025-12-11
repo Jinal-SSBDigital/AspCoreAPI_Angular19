792493529ad6fbfda2d5e4fd125f45fb9946b664
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2025-12-11 17:49:05 +0530

    Add GetFaculty endpoint and update constructor
    Introduced a new GET endpoint 'bindfaculty' to fetch active faculties from the database. Updated the controller constructor to inject AppDbContext and refactored the dwnldRegForm method to adjust parameters for GetStudentDetails.

--- BSEB_CoreAPI/Controllers/dwnldRegFormController.cs
+++ BSEB_CoreAPI/Controllers/dwnldRegFormController.cs
@@ -1,8 +1,12 @@
-﻿using BSEB_CoreAPI.Model;
+﻿using BSEB_CoreAPI.Data;
+using BSEB_CoreAPI.Model;
 using BSEB_CoreAPI.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Entity;
 
 namespace BSEB_CoreAPI.Controllers
 {
@@ -12,17 +16,43 @@ namespace BSEB_CoreAPI.Controllers
     public class dwnldRegFormController : ControllerBase
     {
         private readonly IdwnldRegFormService _dwnldRegFormService;
-        public dwnldRegFormController(IdwnldRegFormService dwnldRegFormService)
+        private readonly AppDbContext _context;
+        public dwnldRegFormController(IdwnldRegFormService dwnldRegFormService, AppDbContext context)
         {
             _dwnldRegFormService = dwnldRegFormService;
+            _context = context;  
+
         }
+
+        [AllowAnonymous]
+        [HttpGet("bindfaculty")]
+        public async Task<IActionResult> GetFaculty()
+        {
+            try
+            {
+                var faculties= await _context.Faculty_Mst.Where(f=>f.IsActive==true).ToListAsync();
+                return Ok(new { success = true, data = faculties });
+                //return Ok(faculties);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+     
+        }
+
         [AllowAnonymous]
         [HttpPost("dwnldRegForm")]
         public async Task<IActionResult> dwnldRegForm([FromBody]StudentRequest Stu)
         {
             try
             {
-                var result = await _dwnldRegFormService.GetStudentDetails(Stu.CollegeId.ToString(),Stu.CollegeCode,Stu.StudentName,Stu.FacultyId.ToString());
+                //if (Stu.CollegeId == "0")
+                //{
+                //    Stu.CollegeId == "";
+                //}
+                var result = await _dwnldRegFormService.GetStudentDetails("",Stu.CollegeCode,Stu.StudentName,Stu.faculty.ToString());
                 return Ok(new { success = true, data = result });
 
             }
