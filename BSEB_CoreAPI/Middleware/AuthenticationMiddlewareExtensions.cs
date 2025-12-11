bccace18e925c0345bb70ca940281399100f989d
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2025-12-11 17:49:11 +0530

    Add AuthenticationMiddlewareExtensions class
    Introduces a static extension class to simplify adding the custom authentication middleware to the ASP.NET Core pipeline via UseCustomAuthentication.

---
+++ BSEB_CoreAPI/Middleware/AuthenticationMiddlewareExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace BSEB_CoreAPI.Middleware
+{
+   
+        public static class AuthenticationMiddlewareExtensions
+        {
+            public static IApplicationBuilder UseCustomAuthentication(this IApplicationBuilder builder)
+            {
+                return builder.UseMiddleware<AuthenticationMiddleware>();
+            }
+        }
+    
+}
