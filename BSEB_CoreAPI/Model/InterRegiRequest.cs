85d7d06289515eae4356f1a3230af5386e9696de
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2025-12-16 15:39:19 +0530

    changes

---
+++ BSEB_CoreAPI/Model/InterRegiRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace BSEB_CoreAPI.Model
+{
+    public class InterRegiRequest
+    {
+        public List<int> StudentIds { get; set; } = new();
+
+        public int CollegeId { get; set; }
+        public int FacultyId { get; set; }
+     }
+}
