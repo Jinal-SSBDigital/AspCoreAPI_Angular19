0b7627510261543799e1bd2eeb9a9ef82397fe34
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2026-01-03 17:38:47 +0530

    Add StudentWithSubjectsDto and SubjectPaperDto models
    Introduces StudentWithSubjectsDto to encapsulate a student and their associated subjects, along with the SubjectPaperDto class to represent subject paper details. These DTOs will facilitate data transfer for student and subject information.

---
+++ BSEB_CoreAPI/Model/StudentWithSubjectsDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BSEB_CoreAPI.Model
+{
+    public class StudentWithSubjectsDto
+    {
+        public StudentInterRegiSpDto Student { get; set; }
+
+        public List<SubjectPaperDto> Subjects { get; set; } = new();
+    }
+
+    public class SubjectPaperDto
+    {
+
+        public int? Pk_SubjectPaperId { get; set; }
+        public int? Fk_SubjectGroupId { get; set; }
+        public string? SubjectPaperName { get; set; }
+        public int? SubjectPaperCode { get; set; }
+        public string? GroupName { get; set; }
+        public string? GroupNameHindi { get; set; }
+    }
+}
