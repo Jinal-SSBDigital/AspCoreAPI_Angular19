4615da97588133f1dc2a2cae055039feca1a8f07
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2026-01-05 17:37:36 +0530

    Update InterRegistrationForm return type in interface
    Changed the return type of InterRegistrationForm from List<Student_Mst> to List<StudentWithSubjectsDto> in the IdwnldRegFormService interface to reflect updated data requirements.

--- BSEB_CoreAPI/Services/IdwnldRegFormService.cs
+++ BSEB_CoreAPI/Services/IdwnldRegFormService.cs
@@ -5,6 +5,6 @@ namespace BSEB_CoreAPI.Services
     public interface IdwnldRegFormService
     {
         Task <List<Student_Mst>> GetStudentDetails(string Collegeid,String CollegeCode,string StudentName,string FacultyId);
-        Task <List<Student_Mst>> InterRegistrationForm(InterRegiRequest InterRegi);
+        Task <List<StudentWithSubjectsDto>> InterRegistrationForm(InterRegiRequest InterRegi);
     }
 }
